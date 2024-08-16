b544df17d65ba5cc26cdc5f243f0479bac3b0933
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-08-16 22:42:00 +0300

    Tehtävä 1.4

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -8,36 +8,42 @@ const Part = (props) => (
 
 const Content = (props) => (
   <>
-    <Part info={props.part1} />
-    <Part info={props.part2} />
-    <Part info={props.part3} />
+    <Part info={props.parts[0]} />
+    <Part info={props.parts[1]} />
+    <Part info={props.parts[2]} />
   </>
 )
 
-const Total = (props) => (
-  <p>Number of exercises {props.count}</p>
-)
+const Total = (props) => {
+  const parts = props.parts
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+    {
       name: 'Fundamentals of React',
       exercises: 10
-  }
-  const part2 = {
+    },
+    {
       name: 'Using props to pass data',
       exercises: 7
-  }
-  const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
-  }
+    }
+  ]
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total count={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   )
 }
