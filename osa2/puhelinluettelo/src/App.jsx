807056a0d8e8af5157d9045c4496dde72cd61732
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-08-23 05:22:00 +0300

    Tehtävä 2.10

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,33 @@
 import { useState } from 'react'
 
+const Filter = ({ string, onChange }) => (
+      <div>
+        filter shown with<input value={string} onChange={onChange} />
+      </div>
+)
+
+const PersonForm = ({ onSubmit, name, onNameChange, number, onNumChange }) => (
+  <form onSubmit={onSubmit}>
+    <div>
+      name: <input value={name} onChange={onNameChange} />
+    </div>
+    <div>
+      number: <input value={number} onChange={onNumChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = ({ persons }) => (
+  <>
+    {persons.map(person =>
+      <div key={person.name}>{person.name} {person.number}</div>
+    )}
+  </>
+)
+
 const App = () => {
   const [persons, setPersons] = useState([
     {
@@ -50,26 +78,15 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with<input value={filterStr}
-                                onChange={handleFilterChange} />
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personsToShow.map(person =>
-        <div key={person.name}>{person.name} {person.number}</div>
-      )}
+      <Filter string={filterStr} onChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm onSubmit={addPerson}
+                  name={newName} onNameChange={handleNameChange}
+                  number={newNumber} onNumChange={handleNumberChange} />
+
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </>
   )
 }
