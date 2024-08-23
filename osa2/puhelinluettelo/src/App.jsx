634c0a19c101410e0090e66362bc240f6dfeaafe
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-08-23 12:16:48 +0300

    Tehtävä 2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ string, onChange }) => (
       <div>
@@ -29,27 +30,16 @@ const Persons = ({ persons }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1231244'
-    },
-    {
-      name: 'Ada Lovelace',
-      number: '39-44-5323523'
-    },
-    {
-      name: 'Dan Abramov',
-      number: '12-43-234345'
-    },
-    {
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122'
-    }
-  ])
+  const [persons, setPersons] = useState([])
   const [filterStr, setFilterStr] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const addPerson = event => {
     event.preventDefault()
